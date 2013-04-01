d2741a1e0d3fc01959705bd5c54e00a981baea68
Peter <peter.m.brandt@gmail.com>
2013-04-01 09:04:01 -0400

    incorporating qunit

--- tests/test-stringbuilder.js
+++ tests/test-stringbuilder.js
@@ -1,8 +1,11 @@
 sb = require('../lib/stringbuilder.js');
 
-
 /*
  * Test adding static objects ------------------------------------------
  */
-
-
+test("sb.push(?)", function() {
+	sb.push('go irish');
+	equal(sb.toString(), 'go irish');
+	sb.push(' beat trojans');
+	equal(sb.toString9), 'go irish beat trojans');
+});
